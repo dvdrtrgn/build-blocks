5ae99e0bf6db8d3c7ca6b31911c735807ed9109c
dvdrtrgn <dvdrtrgn@gmail.com>
2021-05-19 12:46:03 -0500

    Pull in updates from midicalc lib

--- src/guitar/makeNote.js
+++ src/guitar/makeNote.js
@@ -1,58 +1,62 @@
 const NOTES = 'A AB B C CD D DE E F FG G GA'.split(' ');
-const SHARP = !true;
+const FLAT = true;
 const SYMBOL = {
   sharp: '♯',
   flat: '♭',
   natural: '♮',
 };
+const MIN = 12;
+const MAX = 120;
 
 function freqFromMidi(midi) {
-  return Math.pow(2, (midi - 69) / 12) * 440;
+  return (2 ** ((midi - 69) / 12) * 440).toPrecision(4);
 }
 
 function octaveFromMidi(midi) {
   return Math.floor((midi - 12) / 12);
 }
 
-function noteFromMidi(midi) {
-  return NOTES[(midi - 21) % 12];
+function nameFromMidi(midi) {
+  return NOTES[(midi + 3) % 12];
 }
 
 function asSharp(str) {
-  return str.length > 1 ? str[0] + SYMBOL.sharp : str[0];
+  return str.length > 1 ? str[0] + SYMBOL.sharp : str[0] + SYMBOL.natural;
 }
 
 function asFlat(str) {
-  return str.length > 1 ? str[1] + SYMBOL.flat : str[0];
+  return str.length > 1 ? str[1] + SYMBOL.flat : str[0] + SYMBOL.natural;
 }
 
 function normalize(num) {
-  var norm = num;
-  if (num < 21) norm = 21;
-  else if (num > 128) norm = 128;
-  if (norm !== num) console.warn(`Nomalizing ${num} to ${norm}`);
-  return norm;
+  let norm = num;
+  if (num < MIN) norm = MIN;
+  else if (num > MAX) norm = MAX;
+  if (norm !== num) console.warn(`Normalizing ${num} to ${norm}`);
+  return Math.round(norm);
 }
 
 function makeNote(num) {
-  num = normalize(num);
-  let midi = Number(num);
-  let name = noteFromMidi(midi);
+  const midi = normalize(num);
+  const name = nameFromMidi(midi);
 
   return {
-    midi,
-    note: SHARP ? asSharp(name) : asFlat(name),
+    name: makeNote.flat ? asFlat(name) : asSharp(name),
     octave: octaveFromMidi(midi),
     frequency: freqFromMidi(midi),
-    asSharp: asSharp(name),
-    asFlat: asFlat(name),
+    midi,
+    // asSharp: asSharp(name),
+    // asFlat: asFlat(name),
   };
 }
+
+makeNote.min = MIN;
+makeNote.max = MAX;
+makeNote.flat = FLAT;
+makeNote.symbol = SYMBOL;
 
 export default makeNote;
 
 /*
-
-
 
 */
