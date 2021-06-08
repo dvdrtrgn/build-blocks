89fe53600681d38586c07481ba7b3834346e2859
dvdrtrgn <dvdrtrgn@gmail.com>
2021-06-08 15:07:43 -0500

    Document and clean

--- src/libs/PixelStat.js
+++ src/libs/PixelStat.js
@@ -1,17 +1,22 @@
+/*
+  PixelStat (by dvdrtrgn 2021-06-08)
+
+  Make new PixelStat for viewport properties
+  Call watchWindow([element]) have it always updating
+  [if you send in a dom element it will receive classes]
+
+*/
 const W = window;
-const D = document.documentElement;
+const DE = document.documentElement;
 
-const vw = () => Math.max(D.clientWidth || 0, W.innerWidth || 0);
-const vh = () => Math.max(D.clientHeight || 0, W.innerHeight || 0);
-
-function round(num) {
-  return Number(num.toFixed(5));
-}
+const Round = num => Number(num.toFixed(5));
+const VH = () => Math.max(DE.clientHeight || 0, W.innerHeight || 0);
+const VW = () => Math.max(DE.clientWidth || 0, W.innerWidth || 0);
 
 class Pixels {
-  classes = null;
+  #ele = DE;
   #resets = false;
-  #ele = D;
+  classes = null;
 
   #resetEl = function(oldList, newList) {
     if (oldList) oldList.forEach(e => this.#ele.classList.remove(e));
@@ -32,16 +37,35 @@ class Pixels {
     this.update();
   }
 
-  watchWindow(arg) {
-    if (arg && arg.parentElement) {
-      this.#ele = arg;
-    }
-    this.#updateEl(arg);
-    W.addEventListener('resize', this.update);
+  get max() {
+    return Math.max(VW(), VH());
   }
-  unwatchWindow() {
-    W.removeEventListener('resize', this.update);
-    this.#updateEl(false);
+  get min() {
+    return Math.min(VW(), VH());
+  }
+
+  get pxRatio() {
+    return Number(W.devicePixelRatio || 1); // no zoom change on safari
+  }
+  get whRatio() {
+    return Round(VW() / VH());
+  }
+
+  get cssArea() {
+    return (VW() * VH()) / 1e6;
+  }
+  get devArea() {
+    return Round(this.cssArea * this.pxRatio);
+  }
+
+  get clientSize() {
+    return [DE.clientWidth, DE.clientHeight];
+  }
+  get innerSize() {
+    return [W.innerWidth, W.innerHeight];
+  }
+  get screenSize() {
+    return [W.screen.width, W.screen.height];
   }
 
   update() {
@@ -52,6 +76,18 @@ class Pixels {
       this.classSize(),
     ];
     this.#updateEl();
+  }
+
+  watchWindow(arg) {
+    this.#updateEl(false); // clear any previous
+    if (arg && arg.childNodes) this.#ele = arg;
+
+    this.#updateEl(true);
+    W.addEventListener('resize', this.update);
+  }
+  unwatchWindow() {
+    W.removeEventListener('resize', this.update);
+    this.#updateEl(false);
   }
 
   classMega() {
@@ -113,50 +149,6 @@ class Pixels {
 
     return arr.join('\n');
   }
-
-  get max() {
-    return Math.max(vw(), vh());
-  }
-  get min() {
-    return Math.min(vw(), vh());
-  }
-  get pxRatio() {
-    return Number(W.devicePixelRatio || 2);
-  }
-  get whRatio() {
-    return round(vw() / vh());
-  }
-  get cssArea() {
-    return (vw() * vh()) / 1e6;
-  }
-  get devArea() {
-    return round(this.cssArea * this.pxRatio);
-  }
-  get clientSize() {
-    return [D.clientWidth, D.clientHeight];
-  }
-  get innerSize() {
-    return [W.innerWidth, W.innerHeight];
-  }
-  get screenSize() {
-    return [W.screen.width, W.screen.height];
-  }
-
-  // LESS USEFUL STUFF
-  //
-  // get avail() { /* (minus dock and menu) */
-  //   let w = W.screen.availWidth; let h = W.screen.availHeight;
-  //   return `avail-${w}-${h}`;
-  // }
-  // get outer() { /* (basically avail) */
-  //   let w = W.outerWidth; let h = W.outerHeight;
-  //   return `outer-${w}-${h}`;
-  // }
-  // get orient() { // deprecated, no good on safari
-  //   let deg = Math.abs(W.orientation);
-  //   return deg - 90 == 0 ? 'landscape' : 'portrait';
-  // }
-  //
 }
 
 export default Pixels;
