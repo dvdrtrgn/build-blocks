70c7181010fa658f2b3ab73714aeb2cb31cd6540
dvdrtrgn <dvdrtrgn@gmail.com>
2021-07-26 11:14:03 -0500

    Fuss with controls

--- src/libs/music/music.js
+++ src/libs/music/music.js
@@ -12,33 +12,17 @@ Tone.Transport.bpm.value = 150;
 
 const MAX = 0;
 
-function hasStarted() {
-  return Tone.Transport.state === 'started';
-}
-
-function tryToggle() {
-  if (!this.started) {
-    Tone.start().then(() => {
-      Tone.Transport.start();
-    });
-    this.started = true;
-  } else {
-    Tone.Transport.stop();
-    this.started = false;
-  }
-}
-
 let IDX = 0;
 let list;
 
-function dispose() {
+function clearEvents() {
   Tone.Transport.cancel(0);
 }
 
-function remakeList() {
+function scheduleEvents() {
+  clearEvents(1);
   list = {
-    '.': 'music_misc' + IDX++,
-    Tone,
+    '.': 'music_list' + IDX++,
     highChordPart: makeHigh(MAX - 10),
     normChordPart: makeNorm(MAX - 25),
     melodyPart: makeMelody(MAX - 15),
@@ -46,17 +30,15 @@ function remakeList() {
     bassPart: makeBass(MAX - 0),
     kickPart: makeKick(MAX - 10),
   };
-  // window[list['.']] = list;
+  window[list['.']] = list;
   console.log(list);
 }
 
-remakeList();
+scheduleEvents();
 
 export { list };
 
 export default {
-  started: hasStarted(),
-  tryToggle,
-  remakeList,
-  dispose
+  scheduleEvents,
+  clearEvents,
 };
--- src/views/Music.vue
+++ src/views/Music.vue
@@ -1,42 +1,64 @@
 <template>
   <section id="Music">
-    <button @click="toggle" :class="{ active: playing }">
-      Play/Pause
+    <button @click="toggle" :class="{ active: started }">
+      {{ started ? 'stop' : 'start' }}
     </button>
-    <button @click="music.remakeList">remakeList</button>
-    <button @click="music.dispose">dispose</button>
-    <Foo />
+    <button @click="pause">pause</button>
+    {{ transport.state }}
+    <hr>
+    <button @click="music.scheduleEvents">scheduleEvents</button>
+    <button @click="music.clearEvents">clearEvents</button>
+    <label for="">
+      <input type="range" min="1" max="100" v-model.number="volume" />
+      {{ volume }}
+    </label>
   </section>
 </template>
 
 <script>
-  import music, { list } from '@/libs/music/music';
-  import Foo from '@/components/foo';
   import * as Tone from 'tone';
   window.Tone = Tone;
+  import music, { list } from '@/libs/music/music';
 
   export default {
-    components: { Foo },
+    components: {},
     mounted() {},
     data() {
       return {
         music,
+        volume: 51,
+        transport: Tone.Transport,
       };
     },
     beforeCreate() {
       console.log('beforeCreate');
     },
+    created() {
+      this.volume -= 1;
+    },
     beforeDestroy() {
       console.log('beforeDestroy');
     },
     methods: {
-      toggle: function() {
-        this.music.tryToggle();
+      toggle() {
+        this.transport.toggle();
+      },
+      pause() {
+        this.transport.pause();
       },
     },
     computed: {
-      playing() {
-        return this.music.started;
+      list() {
+        return list;
+      },
+      started() {
+        return this.transport.state === 'started';
+      },
+    },
+    watch: {
+      volume() {
+        let db = this.volume / 3 - 33;
+        Tone.Destination.volume.value = db;
       },
     },
   };
@@ -46,8 +68,12 @@
   #Music {
     line-height: 2;
     text-align: center;
+
     .active {
       font-weight: 800;
     }
+  }
+  .hand {
+    z-index: -1;
   }
 </style>
